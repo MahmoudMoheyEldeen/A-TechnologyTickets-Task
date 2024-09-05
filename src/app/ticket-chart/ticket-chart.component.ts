049d335eabf146226a74ae236a91dc41d10ef61f
MahmoudMoheyEldeen <mahmoudmohey1992@gmail.com>
2024-09-05 13:32:39 +0300

    installed echarts

--- src/app/ticket-chart/ticket-chart.component.ts
+++ src/app/ticket-chart/ticket-chart.component.ts
@@ -1,30 +1,36 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NgxEchartsDirective, provideEcharts } from 'ngx-echarts';
-import { EChartsOption } from 'echarts';
+import * as echarts from 'echarts';
 
 @Component({
   selector: 'app-ticket-chart',
   standalone: true,
-  imports: [CommonModule, NgxEchartsDirective],
+  imports: [CommonModule],
   templateUrl: './ticket-chart.component.html',
   styleUrls: ['./ticket-chart.component.scss'],
-  providers: [provideEcharts()],
 })
-export class TicketChartComponent {
-  chartOption: EChartsOption = {
-    xAxis: {
-      type: 'category',
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-    },
-    yAxis: {
-      type: 'value',
-    },
-    series: [
-      {
-        data: [820, 932, 901, 934, 1290, 1330, 1320],
-        type: 'line',
-      },
-    ],
-  };
+export class TicketChartComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    // var chartDom = document.getElementById('main');
+    // var myChart = echarts.init(chartDom);
+    // var option;
+    // option = {
+    //   xAxis: {
+    //     type: 'category',
+    //     boundaryGap: false,
+    //     data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    //   },
+    //   yAxis: {
+    //     type: 'value',
+    //   },
+    //   series: [
+    //     {
+    //       data: [820, 932, 901, 934, 1290, 1330, 1320],
+    //       type: 'line',
+    //       areaStyle: {},
+    //     },
+    //   ],
+    // };
+    // option && myChart.setOption(option);
+  }
 }
