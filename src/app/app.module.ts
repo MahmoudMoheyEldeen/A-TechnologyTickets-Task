3a59b200309b9dfab554b4ce2fbf526a2713bbc6
MahmoudMoheyEldeen <mahmoudmohey1992@gmail.com>
2024-09-05 12:41:32 +0300

    installed qrCode library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TicketChartComponent } from './ticket-chart/ticket-chart.component';
+import { TicketComponent } from './ticket/ticket.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, TicketChartComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    TicketChartComponent,
+    TicketComponent,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/ticket/ticket.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { QRCodeModule } from 'angularx-qrcode';
+
+@Component({
+  selector: 'app-ticket',
+  standalone: true,
+  imports: [CommonModule, QRCodeModule],
+  templateUrl: './ticket.component.html',
+  styleUrls: ['./ticket.component.scss'],
+})
+export class TicketComponent {}
