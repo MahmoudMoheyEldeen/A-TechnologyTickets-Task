63f364a50c1d3e95b22911038415986f15009b3e
MahmoudMoheyEldeen <mahmoudmohey1992@gmail.com>
2024-09-07 19:02:25 +0300

    finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,49 +11,24 @@ import { SearchService } from './services/search.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { InputSwitchModule } from 'primeng/inputswitch';
 import { HttpClientModule } from '@angular/common/http';
-
-// Import Transloco and its modules
-import {
-  TRANSLOCO_CONFIG,
-  TRANSLOCO_LOADER,
-  TranslocoDirective,
-  TranslocoModule,
-  TranslocoPipe,
-} from '@jsverse/transloco'; // Adjust based on your Transloco package path
-import { TranslocoHttpLoader } from './transloco-loader'; // Custom loader
+import { TranslocoRootModule } from './transloco-root.module';
+import { TranslocoDirective, TranslocoPipe } from '@jsverse/transloco';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    TicketChartComponent,
+    TicketComponent,
+    TableComponent,
     ReactiveFormsModule,
     FormsModule,
     InputSwitchModule,
     HttpClientModule,
-    TranslocoModule, // Import TranslocoModule
+    TranslocoRootModule,
     TranslocoDirective,
     TranslocoPipe,
-    TicketChartComponent,
-    TicketComponent,
-    TableComponent,
-    SearchPipe,
-  ],
-  providers: [
-    {
-      provide: TRANSLOCO_CONFIG,
-      useValue: {
-        availableLangs: ['ar', 'en'], // Add your available languages here
-        defaultLang: 'ar', // Set your default language
-        fallbackLang: 'ar', // Fallback language if the translation is missing
-        reRenderOnLangChange: true, // Re-render the component when language changes
-        prodMode: true, // Set to true in production
-      },
-    },
-    {
-      provide: TRANSLOCO_LOADER,
-      useClass: TranslocoHttpLoader, // Use your custom loader to load translations
-    },
   ],
   bootstrap: [AppComponent],
 })
