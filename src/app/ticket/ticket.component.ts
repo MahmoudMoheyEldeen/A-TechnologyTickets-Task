85b289f0f1c3fe856b8b1377c6cbf749ed905767
MahmoudMoheyEldeen <mahmoudmohey1992@gmail.com>
2024-09-07 12:47:15 +0300

    sweetalert

--- src/app/ticket/ticket.component.ts
+++ src/app/ticket/ticket.component.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { QRCodeModule } from 'angularx-qrcode';
 import { TranslocoDirective, TranslocoPipe } from '@jsverse/transloco';
 import { TranslationService } from '../services/translation.service';
+import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-ticket',
   standalone: true,
@@ -24,6 +26,14 @@ export class TicketComponent {
       .writeText(code)
       .then(() => {
         console.log('Copied to clipboard:', code);
+        Swal.fire({
+          position: 'top-right',
+          title: 'Link Copied',
+          showConfirmButton: false,
+          timer: 500,
+          width: 500,
+          heightAuto: true,
+        });
       })
       .catch((err) => {
         console.error('Could not copy text: ', err);
