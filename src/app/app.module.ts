79b945b37f991d49300a422daf1dae542acc2f71
MahmoudMoheyEldeen <mahmoudmohey1992@gmail.com>
2024-09-07 15:43:05 +0300

    fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { InputSwitchModule } from 'primeng/inputswitch';
 import { HttpClientModule } from '@angular/common/http';
 import { TranslocoRootModule } from './transloco-root.module';
-import { environment } from '../environments/environment';
 
 import {
   TRANSLOCO_CONFIG,
